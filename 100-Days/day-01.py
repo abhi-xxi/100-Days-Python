9ac9eb87106de4fdfe5940a924fbbf3df59e2d13
abhi-xxi <abhixxi01@gmail.com>
2025-12-01 10:34:46 +0530

    Update day-01.py

--- 100-Days/day-01.py
+++ 100-Days/day-01.py
@@ -10,10 +10,22 @@
 # print("Heyy "+ input("Write your name ? "))
 
 # #variable
-# name = input("What is your name? ")
+# name = input(" What is your name? ")
 # print(name)
 #
 # # Using len() function
-#
 # print(len(input("Write your name !")))
 
+
+# Day-01 project
+# Band Name Generator Project
+# Description
+# 1. Create a greeting for your program
+# 2. Ask the user for the city that they grew up in and store it in a variable
+# 3. Ask the user for the name of a pet and store it in a variable
+# 4. Combine the name of their city and pet and show them their band name
+
+print("Welcome to the band generator !")
+city = input("Enter the city name you grew up in : ")
+pet = input("Enter the name of the pet : ")
+print("Your band is " + city + " "+ pet)
