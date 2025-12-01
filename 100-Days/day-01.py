894c753041eea75082e60feb055c051211d8394f
abhi-xxi <abhixxi01@gmail.com>
2025-12-01 10:23:05 +0530

    Add files via upload

---
+++ 100-Days/day-01.py
@@ -0,0 +1,19 @@
+# Print first code of python
+# print("Hello World!")
+#
+# # String Manipulation
+# print(" Hello \n world")
+#
+# print("Heyy" + " " + "luffy")   # concat two string
+#
+# # Input -> using input() function
+# print("Heyy "+ input("Write your name ? "))
+
+# #variable
+# name = input("What is your name? ")
+# print(name)
+#
+# # Using len() function
+#
+# print(len(input("Write your name !")))
+
