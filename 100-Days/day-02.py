e6fde9608f66587a88b5f00e3e211a97dfefc0b8
abhi-xxi <abhixxi01@gmail.com>
2025-12-03 00:00:02 +0530

    Update day-02.py

--- 100-Days/day-02.py
+++ 100-Days/day-02.py
@@ -1,5 +1,31 @@
-#Subscripting
+# Subscripting
 print("Hello"[0])
 
-#String
+# String
 print("123" + "345")
+
+# Integer = Whole number
+print(123 + 345)
+
+# Large Integers
+print(123_456_789)
+
+# Float = Floating Print Number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
+
+# Type Checking using - type() function
+print(type("Hello"))
+
+# TypeCasting
+print(int("123") + int("345"))
+
+# Exercise
+# BMI Calculator
+height = 1.65
+weight = 70
+bmi = weight / height **2
+print(bmi)
