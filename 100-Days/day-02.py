18369330b81529b45ab09c28e139734531c8966d
abhi-xxi <abhixxi01@gmail.com>
2025-12-03 00:03:32 +0530

    Update day-02.py

--- 100-Days/day-02.py
+++ 100-Days/day-02.py
@@ -29,3 +29,11 @@ height = 1.65
 weight = 70
 bmi = weight / height **2
 print(bmi)
+
+# Day-02 Final project
+print("Welcome to the tip Calculator!")
+bill = float(input("What was the total bill ? "))
+tip = int(input("How much tip would you like ? 10, 12, or 15? "))
+people = int(input("How many people to split the bill ?"))
+bill_with_tip = tip / 100 * bill + bill 
+print(bill_with_tip)
