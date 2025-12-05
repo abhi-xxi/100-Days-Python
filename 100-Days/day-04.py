c889dd966c7abc130457dca1dd34a83fd150fd93
abhi-xxi <abhixxi01@gmail.com>
2025-12-06 05:16:00 +0530

    Update day-04.py

--- 100-Days/day-04.py
+++ 100-Days/day-04.py
@@ -37,3 +37,50 @@ choice = random.randint(0, 3)
 print("Who will pay the bill ? ")
 print("Answer = " + str(cust_name[choice]))
 
+# Day- 04 final project
+# Rock-Paper-Scissor Game
+import random
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+comp_choice = random.randint(0, 2)
+
+rock = r'''
+    _    
+   | |   
+ _ __ ___   ___| | __
+| '__/ _ \ / __| |/ /
+| | | (_) | (__|   < 
+|_|  \___/ \___|_|\_\
+'''
+paper = r'''
+ _ __   __ _ _ __   ___ _ __ 
+| '_ \ / _` | '_ \ / _ \ '__|
+| |_) | (_| | |_) |  __/ |   
+| .__/ \__,_| .__/ \___|_|   
+| |         | |              
+|_|         |_|              
+'''
+scissors = r'''
+       (_)                       
+ ___  ___ _ ___ ___  ___  _ __ ___ 
+/ __|/ __| / __/ __|/ _ \| '__/ __|
+\__ \ (__| \__ \__ \ (_) | |  \__ \
+|___/\___|_|___/___/\___/|_|  |___/
+'''
+
+Game = [rock, paper, scissors]
+
+if user_choice < 0 or user_choice > 2:
+    print("Invalid choice! You lose.")
+else:
+    print("You chose:\n", Game[user_choice])
+    print("Computer chose:\n", Game[comp_choice])
+
+    if user_choice == comp_choice:
+        print("It's a draw!")
+    elif (user_choice == 0 and comp_choice == 2) or \
+         (user_choice == 1 and comp_choice == 0) or \
+         (user_choice == 2 and comp_choice == 1):
+        print("You win!")
+    else:
+        print("You lose!")
