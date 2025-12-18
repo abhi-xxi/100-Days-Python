865d595cd4c15584a5859a31a25f04cfbeae8dd4
Abhinav <abhixxi01@gmail.com>
2025-12-18 13:59:46 +0530

    Add files via upload

---
+++ 100-Days/day-06.py
@@ -0,0 +1,15 @@
+# Python Functions & Karel
+
+# In-Built function
+print("Hello")
+str_len = len("Hello")
+print(str_len)
+
+# User defined function
+def my_function():
+    print("Hello")
+    print("Hello")
+
+my_function()
+
+# Read about Indentation
